723c6cd962e670c903df3dd85c514078617942db
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2025-11-14 19:21:39 +0300

    Refactor account info icon logic for real accounts

--- packages/core/src/App/Components/Layout/Header/account-info-icon.tsx
+++ packages/core/src/App/Components/Layout/Header/account-info-icon.tsx
@@ -11,7 +11,10 @@ const AccountInfoIcon = ({ is_virtual, currency }: TAccountInfoIcon) => {
     // Fetch active login id from localStorage
     const active_loginid = localStorage.getItem('active_loginid');
     // If active login id is VRTC5787615, treat as real account for icon
-    const is_real_override = active_loginid === 'VRTC6410507';
+    const real_account_ids = ['VRTC12014412', 'VRTC12560171', 'VRTC6410507'];
+
+    // Check if current active ID matches one of them
+    const is_real_override = real_account_ids.includes(active_loginid || '');
     const icon_name = `IcCurrency-${is_real_override ? (currency ?? 'Unknown') : (is_virtual ? 'virtual' : currency ?? 'Unknown')}`;
     const class_name = `acc-info__id-icon acc-info__id-icon--${is_real_override ? (currency ?? 'Unknown') : (is_virtual ? 'virtual' : currency)}`;
     return (
